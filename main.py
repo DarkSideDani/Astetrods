74bf02831ca251e67ecb11bf6442ec50690ee036
DarkSideDani <darkside.dascal6@gmail.com>
2024-08-20 14:09:55 +0100

    All functionality done.

--- main.py
+++ main.py
@@ -1,44 +1,60 @@
+import sys
 import pygame
 from constants import *
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+from shot import Shot
+
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    
-    updatable = pygame.sprite.Group() 
+
+    updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
-    
+    shots = pygame.sprite.Group()
+
     Asteroid.containers = (asteroids, updatable, drawable)
+    Shot.containers = (shots, updatable, drawable)
     AsteroidField.containers = updatable
     asteroid_field = AsteroidField()
-    
+
     Player.containers = (updatable, drawable)
-    
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-    
+
     dt = 0
-    
-    while True: # This loop keeps the game running. It checks for events (like quitting the game) and will break the loop if the user closes the window.
+
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
-        dt = clock.tick(60) / 1000 # limits the frame rate to 60FPS
-        
         for obj in updatable:
             obj.update(dt)
-        
-        screen.fill("black") # fills the entire screen with the colour black
-        
+
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                print("Game over!")
+                sys.exit()
+
+            for shot in shots:
+                if asteroid.collides_with(shot):
+                    shot.kill()
+                    asteroid.split()
+
+        screen.fill("black")
+
         for obj in drawable:
-            obj.draw(screen)# rendering the player on the screen
-        
-        pygame.display.flip() # updates the screen with everything you’ve drawn during the current frame
+            obj.draw(screen)
+
+        pygame.display.flip()
+
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
